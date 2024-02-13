947d5488c953c1ae76a00d3ab69eeb72907748d5
MRINMOY MONDAL <mm2922894@gmail.com>
2024-02-13 21:19:10 +0530

    added fzf-tab

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -8,7 +8,7 @@ export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
 #Plugins
-plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting fzf-tab)
 
 source $ZSH/oh-my-zsh.sh
 
