95dbdd850ed4dd428bb9b5ed97f7a771c92e2117
MRINMOY MONDAL <mm2922894@gmail.com>
2024-05-08 00:26:30 +0530

    fixed biggest threat to vikramnvim "mxsdev/nvim-dap-vscode-js"

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -107,67 +107,10 @@ return {
     opts = {},
   },
 
-  {
-    'mfussenegger/nvim-dap',
-    dependencies = {
-      {
-        'rcarriga/nvim-dap-ui',
-        dependencies = "nvim-neotest/nvim-nio",
-    opts = {},
-      },
-      {
-        "microsoft/vscode-js-debug",
-        opts = true,
-        build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-      },
-  --     {
-  --       "mxsdev/nvim-dap-vscode-js",
-  --       config = function()
-  --         require("dap-vscode-js").setup({
-  --           adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
-  --         })
-  --
-  --         for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
-  --           require("dap").configurations[language] = {
-  --             {
-  --               type = "pwa-node",
-  --               request = "launch",
-  --               name = "Launch file",
-  --               program = "${file}",
-  --               cwd = "${workspaceFolder}",
-  --             },
-  --             {
-  --               type = "pwa-node",
-  --               request = "attach",
-  --               name = "Attach",
-  --               processId = require'dap.utils'.pick_process,
-  --               cwd = "${workspaceFolder}",
-  --             }
-  --           }
-  --         end
-  --       end
-  --     }
-  --   },
-  --   config = function()
-  --     local dap, dapui = require("dap"), require("dapui")
-  --     dap.listeners.before.attach.dapui_config = function()
-  --       dapui.open()
-  --     end
-  --     dap.listeners.before.launch.dapui_config = function()
-  --       dapui.open()
-  --     end
-  --     dap.listeners.before.event_terminated.dapui_config = function()
-  --       dapui.close()
-  --     end
-  --     dap.listeners.before.event_exited.dapui_config = function()
-  --       dapui.close()
-  --     end
-  --   end
-  -- },
-
   --NOTE: Live edit html css javascript
   {
     "turbio/bracey.vim",
+    enabled = false,
     cmd = {"Bracey", "BracyStop", "BraceyReload", "BraceyEval"},
     build = "npm install --prefix server",
   },
