817ee179e6eadcfd577968e198a83463b8826d48
MRINMOY MONDAL <mm2922894@gmail.com>
2024-06-05 00:09:28 +0530

    termux deps updste

--- install/termux-deps.sh
+++ install/termux-deps.sh
@@ -9,24 +9,19 @@ cd /data/data/com.termux/files/usr/etc
 rm motd
 cd
 
-echo -e "${GREEN}...Installing ZSH shell${ENDCOLOR}"
-pkg install zsh 
-echo -e "${GREEN}...Setting ZSH as default shell${ENDCOLOR}"
-chsh -s zsh 
-
 echo -e "${GREEN}...Installing Git${ENDCOLOR}"
 pkg install git
 echo -e "${GREEN}...Installing Curl${ENDCOLOR}"
 pkg install curl
 echo -e "${GREEN}...Installing Wget${ENDCOLOR}"
-pkg install wget
+pkg install wget -y
 echo -e "${GREEN}...Installing NodeJS${ENDCOLOR}"
 plg install nodejs -y
 echo -e "${GREEN}...Installing Neovim${ENDCOLOR}"
 pkg install neovim -y
 echo -e "${GREEN}...Installing Neovim Dependencies${ENDCOLOR}"
 pkg install ripgrep
-pkg install lua-language-server
+pkg install lua-language-server -y
 echo -e "${GREEN}...Installing Tmux${ENDCOLOR}"
 pkg install tmux
 echo -e "${GREEN}...Installing Tmux Dependencies${ENDCOLOR}"
@@ -34,7 +29,7 @@ pkg install ncurses-utils
 echo -e "${GREEN}...Installing Lazygit${ENDCOLOR}"
 pkg install lazygit
 echo -e "${GREEN}...Installing Ruby${ENDCOLOR}"
-pkg install ruby
+pkg install ruby -y
 echo -e "${GREEN}...Installing Entr${ENDCOLOR}"
 pkg install entr
 echo -e "${GREEN}...Installing Zoxide${ENDCOLOR}"
@@ -42,6 +37,11 @@ pkg install zoxide
 echo -e "${GREEN}...Installing Cilorls${ENDCOLOR}"
 gem install colorls
 echo -e "${GREEN}...Installing Pass${ENDCOLOR}"
-pkg install pass
+pkg install pass -y
+
+echo -e "${GREEN}...Installing ZSH shell${ENDCOLOR}"
+pkg install zsh -y 
+echo -e "${GREEN}...Setting ZSH as default shell${ENDCOLOR}"
+chsh -s zsh 
 
 echo -e "${GREEN}...Please Restart Termux${ENDCOLOR}"
