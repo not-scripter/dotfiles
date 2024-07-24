cb1a877ca59275b5eff39508ad9a61b41ce40deb
MRINMOY MONDAL <mm2922894@gmail.com>
2024-07-24 22:19:11 +0530

    migrated to which-key v3

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,94 +1,100 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      default_integrations = true,
-      term_colors = true,
-      transparent_background = true,
-      styles = {
-        comments = {},
-        conditionals = {},
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-      },
-      color_overrides = {
-        mocha = {
-          -- base = "#000000",
-          -- mantle = "#000000",
-          -- crust = "#000000",
-        },
-      },
-      integrations = {
-        cmp = true,
-        ufo = true,
-        gitsigns = true,
-        neotree = true,
-        noice = true,
-        treesitter = true,
-        notify = true,
-        dashboard = true,
-        flash = true,
-        harpoon = true,
-        lsp_trouble = true,
-        which_key = true,
-        telescope = {
-          enabled = true,
-          style = "nvchad",
-        },
-        indent_blankline = {
-          enabled = true,
-          scope_color = "",
-          colored_indent_levels = false,
-        },
-        native_lsp = {
-          enabled = true,
-          virtual_text = {
-            errors = { "italic" },
-            hints = { "italic" },
-            warnings = { "italic" },
-            information = { "italic" },
-          },
-          underlines = {
-            errors = { "underline" },
-            hints = { "underline" },
-            warnings = { "underline" },
-            information = { "underline" },
-          },
-          inlay_hints = {
-            background = true,
-          },
-        },
-      },
-    },
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		opts = {
+			default_integrations = true,
+			term_colors = true,
+			transparent_background = true,
+			styles = {
+				comments = {},
+				conditionals = {},
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+			},
+			color_overrides = {
+				mocha = {
+					-- base = "#000000",
+					-- mantle = "#000000",
+					-- crust = "#000000",
+				},
+			},
+			integrations = {
+				cmp = true,
+				ufo = true,
+				gitsigns = true,
+				neotree = true,
+				noice = true,
+				treesitter = true,
+				notify = true,
+				dashboard = true,
+				flash = true,
+				harpoon = true,
+				lsp_trouble = true,
+				which_key = true,
+				telescope = {
+					enabled = true,
+					style = "nvchad",
+				},
+				indent_blankline = {
+					enabled = true,
+					scope_color = "",
+					colored_indent_levels = false,
+				},
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+					},
+					underlines = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+					},
+					inlay_hints = {
+						background = true,
+					},
+				},
+			},
+		},
+	},
 
-  {
-    "baliestri/aura-theme",
-    config = function(plugin)
-      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-    end
-  },
+	{
+		"baliestri/aura-theme",
+		config = function(plugin)
+			vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+		end,
+	},
 
-  {
-    "folke/tokyonight.nvim",
-    "EdenEast/nightfox.nvim",
-    'projekt0n/github-nvim-theme',
-    "rebelot/kanagawa.nvim",
-    "rose-pine/neovim",
-    "scottmckendry/cyberdream.nvim",
-    "sainnhe/everforest",
-    "navarasu/onedark.nvim",
-    "sainnhe/gruvbox-material",
-    "sainnhe/sonokai",
-    "Mofiqul/dracula.nvim",
-  },
+	{
+		"0xstepit/flow.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = {},
+	},
 
+	{
+		"folke/tokyonight.nvim",
+		"EdenEast/nightfox.nvim",
+		"projekt0n/github-nvim-theme",
+		"rebelot/kanagawa.nvim",
+		"rose-pine/neovim",
+		"scottmckendry/cyberdream.nvim",
+		"sainnhe/everforest",
+		"navarasu/onedark.nvim",
+		"sainnhe/gruvbox-material",
+		"sainnhe/sonokai",
+		"Mofiqul/dracula.nvim",
+	},
 }
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,71 +1,69 @@
-return { 
-  {
-   'ThePrimeagen/vim-be-good'
-  },
+return {
+	{
+		"ThePrimeagen/vim-be-good",
+	},
 
-  --TEST:
-  {
-    "romgrk/nvim-treesitter-context",
-    enabled = false,
-    config = function()
-      require("treesitter-context").setup{
-        enable = true, 
-        throttle = true,
-        max_lines = 0,
-        patterns = { 
-          default = {
-            'class',
-            'function',
-            'method',
-          },
-        },
-      }
-    end
-  },
+	--TEST:
+	{
+		"romgrk/nvim-treesitter-context",
+		enabled = false,
+		config = function()
+			require("treesitter-context").setup({
+				enable = true,
+				throttle = true,
+				max_lines = 0,
+				patterns = {
+					default = {
+						"class",
+						"function",
+						"method",
+					},
+				},
+			})
+		end,
+	},
 
-  {
-    "nvim-telescope/telescope-project.nvim",
-    event = "BufWinEnter",
-    setup = function()
-      vim.cmd [[packadd telescope.nvim]]
-    end,
-  },
+	{
+		"nvim-telescope/telescope-project.nvim",
+		event = "BufWinEnter",
+		setup = function()
+			vim.cmd([[packadd telescope.nvim]])
+		end,
+	},
 
-  --TEST: replacing null-ls
-  -- {
-  --   'mfussenegger/nvim-lint',
-  --   enabled = false,
-  --   config = function()
-  --     local lint = require("lint")
-  --     lint.linters_by_ft = {
-  --       markdown = {'vale',},
-  --       -- javascript = {"eslint"}
-  --     }
-  --
-  --     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  --       callback = function()
-  --         lint.try_lint()
-  --         lint.try_lint("cspell")
-  --       end,
-  --     })
-  --   end
-  -- },
+	--TEST: replacing null-ls
+	-- {
+	--   'mfussenegger/nvim-lint',
+	--   enabled = false,
+	--   config = function()
+	--     local lint = require("lint")
+	--     lint.linters_by_ft = {
+	--       markdown = {'vale',},
+	--       -- javascript = {"eslint"}
+	--     }
+	--
+	--     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	--       callback = function()
+	--         lint.try_lint()
+	--         lint.try_lint("cspell")
+	--       end,
+	--     })
+	--   end
+	-- },
 
-  --TEST: Floating win bar
-  {
-    'b0o/incline.nvim',
-    enabled = false,
-    event = 'VeryLazy',
-    config = function()
-      local helpers = require 'incline.helpers'
-      local navic = require 'nvim-navic'
-      local devicons = require 'nvim-web-devicons'
-      require('incline').setup({
-      })
-    end,
-  },
+	--TEST: Floating win bar
+	{
+		"b0o/incline.nvim",
+		enabled = false,
+		event = "VeryLazy",
+		config = function()
+			local helpers = require("incline.helpers")
+			local navic = require("nvim-navic")
+			local devicons = require("nvim-web-devicons")
+			require("incline").setup({})
+		end,
+	},
 
-  --TEST: music player 
-  --TEST: color picker
-
+	--TEST: music player
+	--TEST: color picker
 }
