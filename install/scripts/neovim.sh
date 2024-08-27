b175eb52ad73cf047f067e52c868f7dc5fb7f425
Mrinmoy <mm2922894@gmail.com>
2024-08-27 21:05:34 +0530

    Create neovim.sh

---
+++ install/scripts/neovim.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+# install DroidSansMono Nerd Font --> u can choose another at: https://www.nerdfonts.com/font-downloads
+echo "[-] Installing Neovim [-]"
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage 
+
+mkdir -p /opt/nvim
+mv nvim.appimage /opt/nvim/nvim 
+
+
+echo "done!"
