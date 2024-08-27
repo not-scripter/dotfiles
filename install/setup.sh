d643bb6dbb4265a86388e993472cc230765ce7b6
MRINMOY MONDAL <mm2922894@gmail.com>
2024-08-27 21:57:49 +0530

    initialized setup script

--- install/setup.sh
+++ install/setup.sh
@@ -1,6 +1,25 @@
-#/bin/bash
-# install DroidSansMono Nerd Font --> u can choose another at: https://www.nerdfonts.com/font-downloads
-bash ~/dotfiles/install/scripts/bootstrap.sh 
+#!/bin/bash
 
-source ~/.zshrc
-echo "done!"
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+echo -e "${GREEN}Setup Begins...${ENDCOLOR}"
+
+cmd_prefix=""
+
+#NOTE: with Case 
+case "$OSTYPE" in
+  darwin*)  echo "OSX" ;; 
+  linux-gnu*)   cmd_prefix="sudo apt" ;;
+  linux-android*)   cmd_prefix="pkg" ;;
+  bsd*)     echo "BSD" ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
+
+if [[ cmd_prefix != "" ]]; then
+  echo -e "${GREEN}OS: $OSTYPE, CMD: $cmd_prefix${ENDCOLOR}" 
+else
+  echo "OS does not decognised"
+fi 
+
