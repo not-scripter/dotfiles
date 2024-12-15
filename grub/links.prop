f1300424fa163f6eab3ffe4150d63f773ed5acc1
MRINMOY MONDAL <mm2922894@gmail.com>
2024-12-15 18:21:52 +0530

    added grub and sddm

---
+++ grub/links.prop
@@ -0,0 +1,3 @@
+$DOTFILES/grub/themes/=/usr/share/grub/themes
+$DOTFILES/grub/grub=/etc/default/grub
+$DOTFILES/grub/40_custom=/etc/grub.d/40_custom #update grub next - sudo grub-mkconfig -o /boot/grub/grub.cfg
--- scripts/ytdlp-segment.sh
+++ scripts/ytdlp-segment.sh
@@ -6,8 +6,9 @@ title="$(yt-dlp --simulate --print "%(title)s" $3)"
 video_url=$links[0]
 audio_url=$links[1]
 
+ffmpeg -ss "$1" -i "$video_url" -ss "$1" -i "$audio_url" -map 0:v -map 1:a -t "$2" -c:v libx264 -c:a aac "$title".mkv
 
-ffmpeg -ss "$1" -i "$video_url" -ss "$1" -i "$audio_url" -map 0:v -map 1:a -ss 30 -t "$2" -c:v libx264 -c:a aac "$title".mkv
+# ffmpeg -ss "$1" -i "$video_url" -ss "$1" -i "$audio_url" -map 0:v -map 1:a -ss 30 -t "$2" -c:v libx264 -c:a aac "$title".mkv
 
 
 
